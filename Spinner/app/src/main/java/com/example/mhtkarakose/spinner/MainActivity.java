868feaea28a96eef5ce411d83b3c6829bb409f58
Mehmet KARAKÖSE <mhtkarakose@gmail.com>
2016-12-31 02:58:34 +0200

    Spinner Example

---
+++ Spinner/app/src/main/java/com/example/mhtkarakose/spinner/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mhtkarakose.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner sp= (Spinner) findViewById(R.id.spinner2);
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position)
+                {
+                    case 0:
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
